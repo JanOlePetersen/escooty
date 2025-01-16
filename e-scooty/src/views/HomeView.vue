60f7a0deb824438dfab908e22b4e258a42a003bb
Kirsten <63075932+Kirsten-Grahl@users.noreply.github.com>
2025-01-16 13:04:24 +0100

    Imported Leaflet

--- e-scooty/src/views/HomeView.vue
+++ e-scooty/src/views/HomeView.vue
@@ -1,9 +1,40 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div style="height:600px; width:800px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 2,
+    };
+  },
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
