0ef48bac2f0e35f415e3b96ae57d71c8fcb14dd4
JanOlePetersen <janole.petersen@gmx.de>
2025-01-16 15:39:40 +0100

    add bootstrap

--- e-scooty/src/components/HelloWorld.vue
+++ e-scooty/src/components/HelloWorld.vue
@@ -3,15 +3,15 @@
     <h1>E Scooty</h1>
         
     <div> <!-- Filter active / completed project partizipation -->
-      <button @click="dropdownInformation = !dropdownInformation"  class="btn btn-outline-dropdown align-items-center mb-2 mb-lg-0 text-decoration-none dropdown-toggle button-size shadow" data-bs-toggle="dropdown" aria-expanded="false" id="filter-active" v-bind:style="{ fontSize: fontSize + 6 + 'px' }">Verkehr</button>
-      <ul v-show="dropdownInformation" @:mouseleave="dropdownInformation = false" class="vue-dropdown-menu list-style-none text-small gap-1 p-2 rounded-3 mx-0 shadow w-220px" id="active-options"  v-bind:style="{ fontSize: fontSize + 4 + 'px' }">
+      <button @click="dropdownInformation = !dropdownInformation"  class="btn btn-outline-dropdown align-items-center mb-2 mb-lg-0 text-decoration-none dropdown-toggle button-size shadow" data-bs-toggle="dropdown" aria-expanded="false" id="filter-active">Verkehr</button>
+      <ul v-show="dropdownInformation" @:mouseleave="dropdownInformation = false" class="vue-dropdown-menu list-style-none text-small gap-1 p-2 rounded-3 mx-0 shadow w-220px" id="traffic-options">
         <li><a class="dropdown-item rounded-2 active" @mousedown="noneLoaded = false" id="alle-projekte" v-on:mouseup="dropdownInformation = !dropdownInformation;" aria-current="page">Alle Filter</a></li>
         <li><a class="dropdown-item rounded-2" @mousedown="noneLoaded = false" v-on:mouseup="dropdownInformation = !dropdownInformation;" id="aktuelle">Parkplätze</a></li>
         <li><a class="dropdown-item rounded-2" @mousedown="noneLoaded = false" v-on:mouseup="dropdownInformation = !dropdownInformation;" id="abgeschlossene">Verbotszonen</a></li>
       </ul>
 
-      <button @click="dropdownActuality = !dropdownActuality"  class="btn btn-outline-dropdown align-items-center mb-2 mb-lg-0 text-decoration-none dropdown-toggle button-size shadow" data-bs-toggle="dropdown" aria-expanded="false" id="filter-active" v-bind:style="{ fontSize: fontSize + 6 + 'px' }">Anderer Filter</button>
-      <ul v-show="dropdownActuality" @:mouseleave="dropdownActuality = false" class="vue-dropdown-menu list-style-none text-small gap-1 p-2 rounded-3 mx-0 shadow w-220px" id="active-options"  v-bind:style="{ fontSize: fontSize + 4 + 'px' }">
+      <button @click="dropdownActuality = !dropdownActuality"  class="btn btn-outline-dropdown align-items-center mb-2 mb-lg-0 text-decoration-none dropdown-toggle button-size shadow" data-bs-toggle="dropdown" aria-expanded="false" id="filter-active" style="margin-left: 10vw">Anderer Filter</button>
+      <ul v-show="dropdownActuality" @:mouseleave="dropdownActuality = false" class="vue-dropdown-menu list-style-none text-small gap-1 p-2 rounded-3 mx-0 shadow w-220px" id="active-options">
         <li><a class="dropdown-item rounded-2 active" @mousedown="noneLoaded = false" id="alle-projekte" v-on:mouseup="dropdownActuality = !dropdownActuality;" aria-current="page">Alle krams</a></li>
         <li><a class="dropdown-item rounded-2" @mousedown="noneLoaded = false" v-on:mouseup="dropdownActuality = !dropdownActuality;" id="aktuelle">confusion</a></li>
         <li><a class="dropdown-item rounded-2" @mousedown="noneLoaded = false" v-on:mouseup="dropdownActuality = !dropdownActuality;" id="abgeschlossene">?!</a></li>
@@ -38,8 +38,6 @@ import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
 import Escooter_Stellplatz from "../assets/Escooter_Stellplatz.geojson";
 import EScooter_Parkverbot_EL from "../assets/EScooter_Parkverbot_EL.geojson";
-// import L from "leaflet";
-// import { onMounted } from "vue";
 
 export default {
   components: {
@@ -66,6 +64,8 @@ export default {
 </script>
 
 <style>
+@import "bootstrap";
+
 html, body {
   margin: 0;
   padding: 0;
