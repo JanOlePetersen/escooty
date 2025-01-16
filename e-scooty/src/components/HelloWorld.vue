168a82a9679da113893341b2e06e2daf5273e916
JanOlePetersen <janole.petersen@gmx.de>
2025-01-16 14:59:56 +0100

    style

--- e-scooty/src/components/HelloWorld.vue
+++ e-scooty/src/components/HelloWorld.vue
@@ -1,5 +1,21 @@
 <template>
-  <div style="height:600px; width:800px">
+  <div>
+    <ul class="nav nav-pills padding-left" style="top: 0">
+      <li class="w-75 mt-2">
+          <h1>E Scooty</h1>
+      </li>
+      <li class="display-right mt-2 pe-5 w-25">
+        <div>
+          <a class="d-inline-flex link-body-emphasis text-decoration-none" id="open-settings">
+            <p>settings</p>
+          </a>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+
+  <div class = "format center">
     <l-map ref="map" v-model:zoom="zoom" :center="[53.75153044444902, 9.664619800111053]">
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -42,5 +58,20 @@ html, body {
 main {
   height: 100vh;
   width: 100vw;
+}
+
+.center {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+
+.format {
+  width: 50vw;
+  height: 50vh;
+}
+
+.display-right {
+  display: right;
 }
 </style>
